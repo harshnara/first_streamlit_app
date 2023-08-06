f743cec6c0db439c6cc4055806ac8474ee5717cf
Hariharanarayanan M <46353703+harshnara@users.noreply.github.com>
2023-08-06 19:19:30 +0530

    Added header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title("My First Streamlit Application")
+
+streamlit.header("Breakfast Menu")
+
+streamlit.text("Masala Dosa")
+streamlit.text("Chole Bhature")
+streamlit.text("Idly")
