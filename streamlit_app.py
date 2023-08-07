2a73632e26fe812189b75803abf248b0c5e72ca0
Hariharanarayanan M <46353703+harshnara@users.noreply.github.com>
2023-08-07 08:50:04 +0530

    Added pandas df

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title("My First Streamlit Application")
 
@@ -7,3 +8,6 @@ streamlit.header("Breakfast Menu")
 streamlit.text("Masala Dosa")
 streamlit.text("Chole Bhature")
 streamlit.text("Idly")
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
